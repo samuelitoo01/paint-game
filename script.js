615d740e303e55279b2dbff0ffe595a66121ccfc
Samuel Sarmiento <samuelsarmiento@MacBook-Air-de-Samuel.local>
2026-01-18 16:18:25 -0500

    mejoras en detalles de la iu

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
 // Crear un paint con canvas 
+document.addEventListener('DOMContentLoaded', () => {
+    const color = document.querySelector('.color');
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+    const buttonRemove = document.querySelector('.btn');
+    let dibujoActivo = false;
 
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+    // Creamos los eventos del mouse
+    function eventosMouse(){
 
-let dibujoActivo = false;
+        canvas.addEventListener('mouseup' , () => {
+            dibujoActivo = false ; 
+            ctx.beginPath();
+        })
 
-// Creamos los eventos del mouse
+        canvas.addEventListener('mousedown' , () => {
+            dibujoActivo = true ;
+        })
 
-function eventosMouse(){
+        canvas.addEventListener('mousemove' , (e) =>{
+            if(!dibujoActivo) return ;
 
-    canvas.addEventListener('mouseup' , () => {
-        dibujoActivo = false ; 
-        ctx.beginPath();
-    })
+            const colorActual = color.value
 
-    canvas.addEventListener('mousedown' , () => {
-        dibujoActivo = true ;
-    })
+            const positionMouseWin = canvas.getBoundingClientRect();
+            // Obtengo la posicion del mouse respecto al canvas
+            let x = e.clientX - positionMouseWin.left
+            let y = e.clientY - positionMouseWin.top
+            // Dibujo las lineas de manera individual para evitar bugs
+            ctx.strokeStyle = colorActual
+            ctx.lineTo(x,y)
+            ctx.stroke()
+            ctx.beginPath()
+            ctx.moveTo(x,y);    
+        })
+    }
 
-    canvas.addEventListener('mousemove' , (e) =>{
-        if(!dibujoActivo) return ;
+    eventosMouse();
 
-        const positionMouseWin = canvas.getBoundingClientRect();
-
-        let x = e.clientX - positionMouseWin.left
-        let y = e.clientY - positionMouseWin.top
-
-        ctx.lineTo(x,y)
-        ctx.stroke()
-        ctx.beginPath()
-        ctx.moveTo(x,y);    
-    })
-}
-
-eventosMouse();
+    // Evento del botón para eliminar todo
+    if (buttonRemove) {
+        buttonRemove.addEventListener('click', function() {
+            ctx.fillStyle = '#FFFFFF';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            ctx.beginPath();
+            dibujoActivo = false;
+        });
+    } else {
+        console.error('Error: No se encontró el botón');
+    }
+})
